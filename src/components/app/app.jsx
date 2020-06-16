bab9396572456931115de727f2b4a72c6d729a0f
Daniel <dm25an22@gmail.com>
2020-06-16 12:48:52 +0200

    add components QuestionGenre/QuestionArtist

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import QuestionGenre from "../question-genre/question-genre.jsx";
+import QuestionArtist from "../question-artist/question-artist.jsx";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 
 const welcomeButtonHandler = () => {};
 
 const App = ({errorsCount}) => {
-  return <WelcomeScreen
-    errorsCount={errorsCount}
-    onWelcomeButtonClick={welcomeButtonHandler}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />;
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
