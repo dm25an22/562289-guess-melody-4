a07be49e5920ca294650fc5c100a6708516f1217
Daniel <dm25an22@gmail.com>
2020-06-26 12:36:37 +0200

    add reducer, initialize store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
+import {createStore} from "redux";
 import ReactDom from "react-dom";
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions.js";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer";
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
+
 ReactDom.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
